218c5109287836a5b58c18da95d9013adf6b8cef
nguyenscg <nguyencsg@gmail.com>
2024-12-15 13:10:55 -0600

    pseudocode higher-lower project

---
+++ higher-lower-game.py
@@ -0,0 +1,13 @@
+# import random module to generate 2 random people from game-data
+# import game-data to use
+# import logo
+
+# A vs B
+# logo here
+# Compare A Structure: {name}, {description}, from {country}.
+# versus logo here
+# Against B Structure: {name}, {description}, from {country}.
+# User input: 'Who has more followers? A or B': 
+# Compare A's followers against B's followers
+# If user guesses correctly, it should +1 score and A becomes B or stays A if the choice was A
+# If user guesses wrong, game ends
