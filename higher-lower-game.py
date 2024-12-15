c2094124729bbe5831b2f5d1b60520739da313d8
nguyenscg <nguyencsg@gmail.com>
2024-12-15 13:17:55 -0600

    randomize 2 person from data

--- higher-lower-game.py
+++ higher-lower-game.py
@@ -1,9 +1,11 @@
-# import random module to generate 2 random people from game-data
+# import random module to generate 2 random people from game_data
 import random
 # import game-data to use
 from game_data import data
 # import logo
 from art import logo
+
+# Create a compare function to compare their followers to each other
 
 # A vs B
 # logo here
@@ -14,3 +16,10 @@ from art import logo
 # Compare A's followers against B's followers
 # If user guesses correctly, it should +1 score and A becomes B or stays A if the choice was A
 # If user guesses wrong, game ends
+
+# get 2 random people from game_data
+person1 = random.choice(data)
+person2 = random.choice(data)
+print(f"{person1}")
+print(f"{person2}")
+
