29400bb47eee621fe25f4923a5aaf6dff220b24f
nguyenscg <nguyencsg@gmail.com>
2024-12-16 15:39:12 -0600

    created a format function to format data

--- higher-lower-game.py
+++ higher-lower-game.py
@@ -15,6 +15,12 @@ from art import logo, vs
 # If user guesses correctly, it should +1 score and A becomes B or stays A if the choice was A
 # If user guesses wrong, game ends
 
+def format_data(person):
+    name = person["name"]
+    description = person["description"]
+    country = person["country"]
+    return f"{name}, a {description}, from {country}"
+
 def compare(user_guess, a_person, b_person):
     """ TAKE THE USER'S GUESS AND PERSON A AND PERSON B'S FOLLOWER COUNTS AND COMPARE THEM. Also checks if the user got the guess correctly. """
     # first compares follower counts, then it will return true or false if the user's guess matches
@@ -24,6 +30,8 @@ def compare(user_guess, a_person, b_person):
         return user_guess == "b"
 
 game_over = False
+score = 0
+
 while not game_over:
     # FIRST STEP: GET 2 RANDOM PEOPLE FROM GAME DATA
     first_person = random.choice(data)
@@ -34,16 +42,16 @@ while not game_over:
     second_person_followers = second_person["follower_count"]
 
     # THIRD STEP: COMPARE THEIR FOLLOWER COUNTS TO EACH OTHER, USE A FUNCTION
-    print(f"{first_person} versus {second_person}")
+    print(f"{format_data(first_person)} versus {format_data(second_person)}")
 
     # FOURTH STEP: Ask the user who has more followers? A or B
     guess = input("Who has more followers? A or B: ").lower()
 
     # Need to see if the user guess correctly
     is_correct = compare(guess, first_person_followers, second_person_followers)
-    score = 0
 
     # check if the user's guess was correct, add +1 if it was, if not then display final score
+    # if correct, if the person B was the answer, it should become person A
     if is_correct:
         score += 1
         print(f"You were right! Score: {score}")
